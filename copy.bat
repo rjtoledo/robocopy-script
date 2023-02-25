631b588719da3d672fbc0349225196182cf27bb3
Rj Toledo <72129826+ddudx@users.noreply.github.com>
2023-02-25 08:59:11 +0700

    Update copy.bat
    Added  a sample of  multiple sources and its destination.

--- copy.bat
+++ copy.bat
@@ -1,13 +1,16 @@
 @echo off
 setlocal
 
-REM Set the source folder you want to copy
-set source_folder=C:\Path\To\Local\Folder
+REM Set the source folders you want to copy to each destination
+set source_folder1=C:\Path\To\Local\Folder1
+set source_folder2=C:\Path\To\Local\Folder2
 
-REM Set the destination folder on the QNAP network drive
-set destination_folder=\\QNAP\Path\To\Network\Folder
+REM Set the destination folders on the QNAP network drive
+set destination_folder1=\\QNAP1\Path\To\Network\Folder1
+set destination_folder2=\\QNAP2\Path\To\Network\Folder2
 
-REM Use Robocopy to copy the folder and all its contents, with progress information
-robocopy "%source_folder%" "%destination_folder%" /e /eta /tee /log:copylog.txt
+REM Use Robocopy to copy the folders and all their contents, with progress information
+robocopy "%source_folder1%" "%destination_folder1%" /e /np /tee /log:copylog1.txt
+robocopy "%source_folder2%" "%destination_folder2%" /e /np /tee /log:copylog2.txt
 
-echo Folder copied successfully!
+echo Folders copied successfully!
